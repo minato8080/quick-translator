09a8f6196739d287f7d92c0da06a98da30e88738
Kaito Fujinuma <sokonashi0124@gmail.com>
2024-11-11 22:39:39 +0900

    divide complex compornents

--- src/components/Flashcard.tsx
+++ src/components/Flashcard.tsx
@@ -175,16 +175,18 @@ const EditableText = ({
  * @param isLearningMode - 学習モードかどうか
  * @param learningMode - 学習モードのタイプ
  */
-const CardCore = ({
+const CardLeef = ({
   flashcardHandler,
-  items,
+  item,
   startIndex = 0,
+  divisionIndex,
   isLearningMode,
   learningMode,
 }: {
   flashcardHandler: ReturnType<typeof useFlashcardHandler>;
-  items: FlashcardType[];
+  item: FlashcardType;
   startIndex?: number;
+  divisionIndex: number;
   isLearningMode: boolean;
   learningMode: LearningMode;
 }) => {
@@ -199,27 +201,112 @@ const CardCore = ({
   } = flashcardHandler;
 
   return (
-    <>
-      {items.map((item, lindex) => (
-        <motion.div
-          key={item.timestamp}
-          initial={{ opacity: 0, y: 20 }}
-          animate={{ opacity: 1, y: 0 }}
-          exit={{ opacity: 0, y: -20 }}
-          transition={{ duration: 0.3 }}
-        >
-          {/* 各翻訳履歴のカード */}
-          <Card className="hover:shadow-md transition-shadow">
-            <CardHeader className="p-3 h-10">
-              <AnimatePresence mode="wait">
-                <div className="flex justify-between items-start mb-1">
-                  {/* 翻訳元の言語表示 */}
-                  <span className="text-[12px] font-medium text-gray-400">
-                    {languages[item.sourceLang]} → {languages[item.targetLang]}
-                  </span>
-                  {/* ボタンを配置する場所*/}
-                  <div className="flex space-x-1">
-                    {isLearningMode ? (
+    <motion.div
+      key={item.timestamp}
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: 1, y: 0 }}
+      exit={{ opacity: 0, y: -20 }}
+      transition={{ duration: 0.3 }}
+    >
+      {/* 各翻訳履歴のカード */}
+      <Card className="hover:shadow-md transition-shadow">
+        <CardHeader className="p-3 h-10">
+          <AnimatePresence mode="wait">
+            <div className="flex justify-between items-start mb-1">
+              {/* 翻訳元の言語表示 */}
+              <span className="text-[12px] font-medium text-gray-400">
+                {languages[item.sourceLang]} → {languages[item.targetLang]}
+              </span>
+              {/* ボタンを配置する場所*/}
+              <div className="flex space-x-1">
+                {isLearningMode ? (
+                  <motion.div
+                    key={
+                      item.editing
+                        ? "editing"
+                        : item.saved
+                        ? "saved"
+                        : "unsaved"
+                    }
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    exit={{ opacity: 0 }}
+                    transition={{ duration: 0.3 }}
+                  >
+                    <div className="flex items-center space-x-2 h-4">
+                      {item.visible ? (
+                        <Eye className="h-4 w-4 text-blue-600" />
+                      ) : (
+                        <EyeOff className="h-4 w-4 text-blue-400" />
+                      )}
+                      <Switch
+                        id="text-area-toggle"
+                        checked={item.visible}
+                        onCheckedChange={(checked) => {
+                          setFlashcards((prev) =>
+                            prev.map((flashcard) =>
+                              flashcard.timestamp === item.timestamp
+                                ? { ...flashcard, visible: checked }
+                                : flashcard
+                            )
+                          );
+                        }}
+                        className="data-[state=checked]:bg-blue-600 data-[state=unchecked]:bg-blue-400"
+                      />
+                    </div>
+                  </motion.div>
+                ) : (
+                  <div className="flex items-center space-x-2 h-4">
+                    <ConfirmDialog title="Save to vocabulary?" ok="Save">
+                      {/* 保存ボタン */}
+                      <Button
+                        variant="ghost"
+                        size="sm"
+                        onClick={() =>
+                          handleSaveTranslation(startIndex + divisionIndex)
+                        }
+                        disabled={item.editing ? false : item.saved}
+                      >
+                        <motion.div
+                          key={
+                            item.editing
+                              ? "unsaved"
+                              : item.saved
+                              ? "saved"
+                              : "unsaved"
+                          }
+                          initial={{ opacity: 0, rotate: -180 }}
+                          animate={{ opacity: 1, rotate: 0 }}
+                          exit={{ opacity: 0, rotate: 180 }}
+                          transition={{ duration: 0.3 }}
+                        >
+                          {item.editing ? (
+                            <Save className="h-4 w-4 text-blue-600" />
+                          ) : item.saved ? (
+                            <Check className="h-4 w-4 text-blue-400" />
+                          ) : (
+                            <Save className="h-4 w-4 text-blue-600" />
+                          )}
+                        </motion.div>
+                        <span className="sr-only">
+                          {item.editing
+                            ? "Save changes"
+                            : item.saved
+                            ? "Saved"
+                            : "Save translation"}
+                        </span>
+                      </Button>
+                    </ConfirmDialog>
+                    {/* 編集ボタン */}
+                    <Button
+                      variant="ghost"
+                      size="sm"
+                      onClick={() =>
+                        item.editing
+                          ? handleCancelEdit()
+                          : handleEditTranslation(startIndex + divisionIndex)
+                      }
+                    >
                       <motion.div
                         key={
                           item.editing
@@ -228,161 +315,110 @@ const CardCore = ({
                             ? "saved"
                             : "unsaved"
                         }
-                        initial={{ opacity: 0 }}
-                        animate={{ opacity: 1 }}
-                        exit={{ opacity: 0 }}
+                        initial={{ opacity: 0, rotate: -180 }}
+                        animate={{ opacity: 1, rotate: 0 }}
+                        exit={{ opacity: 0, rotate: 180 }}
                         transition={{ duration: 0.3 }}
                       >
-                        <div className="flex items-center space-x-2 h-4">
-                          {item.visible ? (
-                            <Eye className="h-4 w-4 text-blue-600" />
-                          ) : (
-                            <EyeOff className="h-4 w-4 text-blue-400" />
-                          )}
-                          <Switch
-                            id="text-area-toggle"
-                            checked={item.visible}
-                            onCheckedChange={(checked) => {
-                              setFlashcards((prev) =>
-                                prev.map((flashcard) =>
-                                  flashcard.timestamp === item.timestamp
-                                    ? { ...flashcard, visible: checked }
-                                    : flashcard
-                                )
-                              );
-                            }}
-                            className="data-[state=checked]:bg-blue-600 data-[state=unchecked]:bg-blue-400"
-                          />
-                        </div>
+                        {item.editing ? (
+                          <RotateCcw className="h-4 w-4 text-teal-600" />
+                        ) : (
+                          <Edit className="h-4 w-4 text-teal-600" />
+                        )}
                       </motion.div>
-                    ) : (
-                      <div className="flex items-center space-x-2 h-4">
-                        <ConfirmDialog title="Save to vocabulary?" ok="Save">
-                          {/* 保存ボタン */}
-                          <Button
-                            variant="ghost"
-                            size="sm"
-                            onClick={() =>
-                              handleSaveTranslation(startIndex + lindex)
-                            }
-                            disabled={item.editing ? false : item.saved}
-                          >
-                            <motion.div
-                              key={
-                                item.editing
-                                  ? "unsaved"
-                                  : item.saved
-                                  ? "saved"
-                                  : "unsaved"
-                              }
-                              initial={{ opacity: 0, rotate: -180 }}
-                              animate={{ opacity: 1, rotate: 0 }}
-                              exit={{ opacity: 0, rotate: 180 }}
-                              transition={{ duration: 0.3 }}
-                            >
-                              {item.editing ? (
-                                <Save className="h-4 w-4 text-blue-600" />
-                              ) : item.saved ? (
-                                <Check className="h-4 w-4 text-blue-400" />
-                              ) : (
-                                <Save className="h-4 w-4 text-blue-600" />
-                              )}
-                            </motion.div>
-                            <span className="sr-only">
-                              {item.editing
-                                ? "Save changes"
-                                : item.saved
-                                ? "Saved"
-                                : "Save translation"}
-                            </span>
-                          </Button>
-                        </ConfirmDialog>
-                        {/* 編集ボタン */}
-                        <Button
-                          variant="ghost"
-                          size="sm"
-                          onClick={() =>
-                            item.editing
-                              ? handleCancelEdit()
-                              : handleEditTranslation(startIndex + lindex)
-                          }
-                        >
-                          <motion.div
-                            key={
-                              item.editing
-                                ? "editing"
-                                : item.saved
-                                ? "saved"
-                                : "unsaved"
-                            }
-                            initial={{ opacity: 0, rotate: -180 }}
-                            animate={{ opacity: 1, rotate: 0 }}
-                            exit={{ opacity: 0, rotate: 180 }}
-                            transition={{ duration: 0.3 }}
-                          >
-                            {item.editing ? (
-                              <RotateCcw className="h-4 w-4 text-teal-600" />
-                            ) : (
-                              <Edit className="h-4 w-4 text-teal-600" />
-                            )}
-                          </motion.div>
-                          <span className="sr-only">
-                            {item.editing ? "Cancel edit" : "Edit translation"}
-                          </span>
-                        </Button>
-                        {/* 翻訳の削除ボタン */}
-                        <ConfirmDialog title="Delete this?" ok="OK">
-                          <Button
-                            variant="ghost"
-                            size="sm"
-                            onClick={() =>
-                              handleDeleteTranslation(startIndex + lindex)
-                            }
-                          >
-                            <X className="h-4 w-4 text-pink-600" />
-                            <span className="sr-only">Delete translation</span>
-                          </Button>
-                        </ConfirmDialog>
-                      </div>
-                    )}
+                      <span className="sr-only">
+                        {item.editing ? "Cancel edit" : "Edit translation"}
+                      </span>
+                    </Button>
+                    {/* 翻訳の削除ボタン */}
+                    <ConfirmDialog title="Delete this?" ok="OK">
+                      <Button
+                        variant="ghost"
+                        size="sm"
+                        onClick={() =>
+                          handleDeleteTranslation(startIndex + divisionIndex)
+                        }
+                      >
+                        <X className="h-4 w-4 text-pink-600" />
+                        <span className="sr-only">Delete translation</span>
+                      </Button>
+                    </ConfirmDialog>
                   </div>
-                </div>
-              </AnimatePresence>
-            </CardHeader>
-            <CardContent className="p-1">
-              {/* 翻訳元テキストの表示または編集 */}
-              <div className="flex justify-between items-center mb-2">
-                <EditableText
-                  io={switchIO(learningMode, item, "input")}
-                  item={item}
-                  editingText={editingText}
-                  setEditingText={setEditingText}
-                  isLearningMode={isLearningMode}
-                  learningMode={learningMode}
-                />
-                <SpeakerButton
-                  text={item[switchIO(learningMode, item, "input")]}
-                  lang={switchSpeaker(learningMode, item, "input")}
-                />
+                )}
               </div>
-              {/* 翻訳先テキストの表示または編集 */}
-              <div className="flex justify-between items-center mb-1">
-                <EditableText
-                  io={switchIO(learningMode, item, "output")}
-                  item={item}
-                  editingText={editingText}
-                  setEditingText={setEditingText}
-                  isLearningMode={isLearningMode}
-                  learningMode={learningMode}
-                />
-                <SpeakerButton
-                  text={item[switchIO(learningMode, item, "output")]}
-                  lang={switchSpeaker(learningMode, item, "output")}
-                />
-              </div>
-            </CardContent>
-          </Card>
-        </motion.div>
+            </div>
+          </AnimatePresence>
+        </CardHeader>
+        <CardContent className="p-1">
+          {/* 翻訳元テキストの表示または編集 */}
+          <div className="flex justify-between items-center mb-2">
+            <EditableText
+              io={switchIO(learningMode, item, "input")}
+              item={item}
+              editingText={editingText}
+              setEditingText={setEditingText}
+              isLearningMode={isLearningMode}
+              learningMode={learningMode}
+            />
+            <SpeakerButton
+              text={item[switchIO(learningMode, item, "input")]}
+              lang={switchSpeaker(learningMode, item, "input")}
+            />
+          </div>
+          {/* 翻訳先テキストの表示または編集 */}
+          <div className="flex justify-between items-center mb-1">
+            <EditableText
+              io={switchIO(learningMode, item, "output")}
+              item={item}
+              editingText={editingText}
+              setEditingText={setEditingText}
+              isLearningMode={isLearningMode}
+              learningMode={learningMode}
+            />
+            <SpeakerButton
+              text={item[switchIO(learningMode, item, "output")]}
+              lang={switchSpeaker(learningMode, item, "output")}
+            />
+          </div>
+        </CardContent>
+      </Card>
+    </motion.div>
+  );
+};
+
+/**
+ * カード本体コンポーネント
+ * @param flashcardHandler - フラッシュカードのハンドラー
+ * @param items - フラッシュカードのアイテムリスト
+ * @param startIndex - 開始インデックス
+ * @param isLearningMode - 学習モードかどうか
+ * @param learningMode - 学習モードのタイプ
+ */
+const CardCore = ({
+  flashcardHandler,
+  items,
+  startIndex = 0,
+  isLearningMode,
+  learningMode,
+}: {
+  flashcardHandler: ReturnType<typeof useFlashcardHandler>;
+  items: FlashcardType[];
+  startIndex?: number;
+  isLearningMode: boolean;
+  learningMode: LearningMode;
+}) => {
+  return (
+    <>
+      {items.map((item, divisionIndex) => (
+        <CardLeef
+          key={startIndex + divisionIndex}
+          startIndex={startIndex}
+          divisionIndex={divisionIndex}
+          flashcardHandler={flashcardHandler}
+          item={item}
+          isLearningMode={isLearningMode}
+          learningMode={learningMode}
+        />
       ))}
     </>
   );
