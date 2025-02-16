676cd4bd5065f96b3d2cb4d963301644abc4d652
Kaito Fujinuma <sokonashi0124@gmail.com>
2025-02-16 23:48:37 +0900

    Add header configures of cache control

---
+++ src/middleware.ts
@@ -0,0 +1,13 @@
+import { NextResponse } from "next/server";
+
+import type { NextRequest } from "next/server";
+
+export function middleware(_request: NextRequest) {
+  const res = NextResponse.next();
+  res.headers.set("Cache-Control", "public, max-age=31536000, immutable");
+  return res;
+}
+
+export const config = {
+  matcher: ["/pwa/:path*", "/favicon.ico"],
+};
